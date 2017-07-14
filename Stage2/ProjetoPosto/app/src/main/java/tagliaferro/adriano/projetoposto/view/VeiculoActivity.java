8358a357d4a2ae4784bb5aab3a32ca1dbd538eba
Adriano Augusto Tagliaferro <adrianotagliaferro@hotmail.com>
2017-07-14 00:15:21 -0300

    Tarefa 2
    Criação da tela de veículo.

---
+++ Stage2/ProjetoPosto/app/src/main/java/tagliaferro/adriano/projetoposto/view/VeiculoActivity.java
@@ -0,0 +1,20 @@
+package tagliaferro.adriano.projetoposto.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import tagliaferro.adriano.projetoposto.R;
+
+/**
+ * Created by Adriano2 on 13/07/2017.
+ */
+
+public class VeiculoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veiculo);
+
+
+    }
+}
